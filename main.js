6bcaf494ecde5a57ea46270ff646493754ce74c5
Rayden Sia Zhen Ron <rayden.szr@singlebuyer.com.my>
2024-01-23 16:37:31 +0800

    Added autoplay functionality. Added images. Can consider automating slideshow image number.

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
-let slideIndex = 0;
-showSlides();
+let slideIndex = 1;
+let timeout;
+showSlides(slideIndex);
 
-function showSlides() {
+// Next/previous controls
+function plusSlides(n = 1, id) {
+  clearTimeout(timeout)
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
+  // let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+  // for (i = 0; i < dots.length; i++) {
+  //   dots[i].className = dots[i].className.replace(" active", "");
+  // }
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 15000); // Change image every 15 seconds
+  timeout = setTimeout(plusSlides, 15000);
+  // dots[slideIndex-1].className += " active";
 }
